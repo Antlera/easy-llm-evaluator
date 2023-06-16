ec8e6fdcdaab05deae52b20c9f5fabb70d687589
tafflan <3219599757@qq.com>
2023-06-16 14:10:36 +0000

    Add easy-to-use startup scripts.

---
+++ eval/script/generate_question.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the variables
+category="$CATEGORY"
+project_name="$PROJECT_NAME"
+srcfile="eval/table/question/$project_name/example_questions.json"
+outfile="eval/table/question/$project_name/questions.jsonl"
+
+# Run the script and pass the arguments
+python -m eval.generate_questions --category "$category" --srcfile "$srcfile" --outfile "$outfile"
